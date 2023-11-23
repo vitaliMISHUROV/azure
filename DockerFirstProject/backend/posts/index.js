3d49ad7b08038910408dc98ab6115dd8b175b2d7
vitaliMISHUROV <vitalamishurov@gmail.com>
2023-11-23 14:38:10 +0200

    Docker API GETWAY маршрутизатор

---
+++ DockerFirstProject/backend/posts/index.js
@@ -0,0 +1,6 @@
+const http = require("http");
+const SERVER_NAME = process.env.SERVER_NAME || "I havennt name "
+http.createServer(function(request, response){
+
+    response.end("<h1>Post Container :"+ SERVER_NAME +" </h1>");
+}).listen(80);
